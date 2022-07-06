94f6bf389e81e80a8e99a2a58a82092618fff35d
nurlykhan <nurlykhan.salamatuly@samgau.com>
2022-07-06 06:34:04 +0600

    add eslint, husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,17 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        semi: 0,
+        'comma-dangle': 0,
+        quotes: [2, 'single', 'avoid-escape'],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'react/jsx-filename-extension': [
+          1,
+          { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+        ],
+        'react/react-in-jsx-scope': 'off',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'max-len': ['error', { code: 140 }],
     }
 }
